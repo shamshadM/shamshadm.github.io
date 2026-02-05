43dd2cc136f1c31f1d7893b440b6c675b246dcd1
M. Shamshad <shamshad.rattan@gmail.com>
2026-02-05 16:19:25 +0530

    update about

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -220,4 +220,16 @@ var store = [{
         "tags": [],
         "url": "/teaching/2018-Graduate%20Teaching%20Assistant",
         "teaser": null
+      },{
+        "title": "Field experimental analysis",
+        "excerpt":"Field experimental analysis is a research method that uses controlled interventions within a real-world setting to study cause-and-effect relationships. Unlike a lab experiment, which maximizes control in an artificial environment, a field experiment prioritizes testing an intervention in a natural setting to improve the real-world applicability of its findings. This...","categories": [],
+        "tags": [],
+        "url": "/_teaching/designs",
+        "teaser": null
+      },{
+        "title": "Summary Descriptive Statistics",
+        "excerpt":"Field experimental analysis is a research method that uses controlled interventions within a real-world setting to study cause-and-effect relationships. Unlike a lab experiment, which maximizes control in an artificial environment, a field experiment prioritizes testing an intervention in a natural setting to improve the real-world applicability of its findings. This...","categories": [],
+        "tags": [],
+        "url": "/_teaching/summary",
+        "teaser": null
       }]
